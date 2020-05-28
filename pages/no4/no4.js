91357b59ae98ea69308c1c295aaa2f3c00c647f1
zy <536408714@qq.com>
2020-05-28 23:41:51 +0800

    v1.0

--- pages/no4/no4.js
+++ pages/no4/no4.js
@@ -1,30 +1,50 @@
-// pages/no4/no4.js
+//获取应用实例
+const app = getApp();
+ 
 Page({
+  /**
+   * 页面的初始数据
+   */
   data: {
-    
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
-
+    var that = this;
+    if (app.globalData.userInfo) {
+      that.setUserInfo(app.globalData.userInfo);
+    } else if (that.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        that.setUserInfo(res.userInfo);
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          that.setUserInfo(res.userInfo);
+        }
+      })
+    }
   },
-  onReady: function () {
-
+ 
+  getUserInfo: function (e) {
+    this.setUserInfo(e.detail.userInfo);
   },
-  onShow: function () {
-
-  },
-  onHide: function () {
-
-  },
-  onUnload: function () {
-
-  },
-  onPullDownRefresh: function () {
-
-  },
-  onReachBottom: function () {
-
-  },
-  onShareAppMessage: function () {
-
+ 
+  setUserInfo: function (userInfo) {
+    if (userInfo != null) {
+      app.globalData.userInfo = userInfo
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      })
+    }
   }
 })

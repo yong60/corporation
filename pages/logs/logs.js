fda0ca0e63b9d38b25316bcff628930e17179316
sweetcoral <g2855322030@163.com>
2020-06-13 01:16:50 +0800

    部分页面进行了美工

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,4 @@
-//logs.js
-const util = require('../../utils/util.js')
-
+const app = getApp()
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
-})
+  
+});

67d6dbba91f6aee9cd60a6f907398b7183806e39
zy <536408714@qq.com>
2020-06-15 01:11:55 +0800

    按钮更新

--- pages/no1/no1.js
+++ pages/no1/no1.js
@@ -1,66 +1,35 @@
 const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    img: [
+      'lun1.jpg',
+      'lun2.jpg',
+      'lun3.jpg'
+    ],
+    indicatorDots:true,
+    //是否显示面板指示点
+    autoplay:true,
+    //是否自动切换
+    interval: 5000,
+    //自动切换时间间隔
+    duration: 500,
+    //滑动动画时长
+    color:'#ffffff',
+    //当前选中的指示点颜色
+    height:''
+    //swiper高度
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goheight:function (e) {
+    var width = wx.getSystemInfoSync().windowWidth
+    //获取可使用窗口宽度
+    var imgheight = e.detail.height
+    //获取图片实际高度
+    var imgwidth = e.detail.width
+    //获取图片实际宽度
+    var height = width * imgheight / imgwidth +"px"
+    //计算等比swiper高度
+    this.setData({
+      height: height
+    })
   }
 })
